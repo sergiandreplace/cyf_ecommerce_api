19d9803b759d2ef4737712941520f0ea94af3eaa
Sergi Martínez <sergi.martinez@scmspain.com>
2020-03-10 20:15:23 +0100

    secret file created

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const app = express();
+const { Pool } = require('pg');
+const secrets = require('./secrets');
+
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'cyf_ecommerce',
+    password: secrets.dbPassword,
+    port: 5432
+});
+
+app.get("/customers", (req, res) => {
+    res.send("potato")
+})
 
 app.listen(3000, function() {
     console.log("Server is listening on port 3000. Ready to accept requests!");
